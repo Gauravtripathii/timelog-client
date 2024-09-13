ac1320d29fbc20f932f411dfac86cd82589f3e19
gauravtripathii <gauravtripathii7979@gmail.com>
2024-09-14 01:08:09 +0530

    profile page mb

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Registeration from './Components/registeration' 
 import TestHome from './Components/TestHome';
 import Home from './Components/Home';
+import Profile from './Components/Profile';
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
       {/* <Registeration /> */}
       {/* <TestHome /> */}
-      <Home />
+      {/* <Home /> */}
+      <Profile />
     </div>
   );
 }
